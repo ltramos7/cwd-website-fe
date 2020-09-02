31abf0c3a3a7325d7294f15a068d31886ab3c8a2
Linda <ltramos7@gmail.com>
2020-09-01 21:25:22 -0500

    added sass file for variables

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component{
     render(){
         return(
-            <h1>Home Component</h1>
+            <h1 className='home'>Home Component</h1>
         )
     }
 }
