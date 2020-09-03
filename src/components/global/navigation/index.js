7ee4ed14e2d74233fc7fe04aa53e6a31ec82bce1
Alejandro Linan <jandrolinan@gmail.com>
2020-09-03 09:33:10 -0500

    nav comp

--- src/components/global/navigation/index.js
+++ src/components/global/navigation/index.js
@@ -3,7 +3,36 @@ import React from 'react';
 class Navigation extends React.Component{
     render(){
         return(
-            <h1>Navigation Component</h1>
+            <header>
+                <ul className='nav-ul'>
+                    <li className='nav-li'>
+                        <a href='/'>
+                            CWD
+                        </a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='/about'>
+                            ABOUT
+                        </a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='/work'>
+                            WORK
+                        </a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='/blog'>
+                            BLOG
+                        </a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='/contact'>
+                            CONTACT
+                        </a>
+                    </li>
+                </ul>
+            </header>
+
         )
     }
 }
