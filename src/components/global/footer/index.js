7e1d41e6cfdcc82e6bf1c5c1de6b5f9590001163
alinan-vn <jandrolinan@gmail.com>
2020-09-09 14:57:06 -0500

    footer comp, no styling yet

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -3,7 +3,73 @@ import React from 'react';
 class Footer extends React.Component{
     render(){
         return(
-            <h1>Footer Component</h1>
+            <footer>
+                <ul>
+                    <li>
+                        <a>
+                            CWD
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Work
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Career
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
+                <hr />
+                <ul>
+                    <li>
+                        <a>
+                            Terms & Conditions
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            FAQ
+                        </a>
+                    </li>
+                </ul>
+            </footer>
         )
     }
 }
