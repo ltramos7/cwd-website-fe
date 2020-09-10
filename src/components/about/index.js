7efade2e5a4310c9fc348bf17d39b6008954ab41
alinan-vn <jandrolinan@gmail.com>
2020-09-10 14:17:54 -0500

    added some about structure and a placeholder image

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,7 +3,28 @@ import React from 'react';
 class About extends React.Component{
     render(){
         return(
-            <h1>About Component</h1>
+            <section>
+                <h1>ABOUT</h1>
+                <div>
+                    <h3>
+                        What we believe
+                    </h3>
+                    <p>
+                        viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis
+                    </p>
+                    <p>
+                        viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis
+                    </p>
+                </div>
+
+                <div>
+                    <div>
+                        <h3>Our Team</h3>
+                        <p>{ '<-- -->' } </p>
+                    </div>
+
+                </div>
+            </section>
         )
     }
 }
