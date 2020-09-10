dca8bb42a959227c13b4087af3e3eb3f53c0ff16
alinan-vn <jandrolinan@gmail.com>
2020-09-10 14:36:02 -0500

    added the rest of the js/html structure, no styling yet

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -23,6 +23,62 @@ class About extends React.Component{
                         <p>{ '<-- -->' } </p>
                     </div>
 
+                    <div>
+                        <img src={require(`./images/rocky-wallpaper.jpg`)} alt='placeholder' />
+                        <h5>Anshika Kala</h5>
+                        <p>Product Designer</p>
+                        <p>+1 (773) 123-1231</p>
+                        <p>kala.anshika@cwd.com</p>
+                    </div>
+                    <div>
+                        <img src={require(`./images/rocky-wallpaper.jpg`)} alt='placeholder' />
+                        <h5>Anshika Kala</h5>
+                        <p>Product Designer</p>
+                        <p>+1 (773) 123-1231</p>
+                        <p>kala.anshika@cwd.com</p>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div>
+                    <h3>Services</h3>
+                    <div>
+                        <div>
+                            <h5>Strategy & Organization</h5>
+                            <p>lorem this is the description la lorem i dont have it for react lorem</p>
+                            <p>Read More</p>
+                        </div>
+                        <div>
+                            <h5>Brand & Advertising</h5>
+                            <p>lorem this is the description la lorem i dont have it for react lorem</p>
+                            <p>Read More</p>
+                        </div>
+                        <div>
+                            <h5>Design & Technology</h5>
+                            <p>lorem this is the description la lorem i dont have it for react lorem</p>
+                            <p>Read More</p>
+                        </div>
+                        <div>
+                            <h5>Lotem Ipsum & Sit</h5>
+                            <p>lorem this is the description la lorem i dont have it for react lorem</p>
+                            <p>Read More</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h1>Contact Us</h1>
+                    <div>
+                        <p>
+                            lorem this is the description la lorem i dont have it for react lorem.lorem this is the description la lorem i dont have it for react lorem
+                        </p>
+                        <ul>
+                            <li>lorem ipsum dolor sit amet</li>
+                            <li>lorem ipsum dolor sit amet</li>
+                            <li>lorem ipsum dolor sit amet</li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         )
