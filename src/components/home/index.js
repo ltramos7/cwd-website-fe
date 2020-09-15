6cf7d102ef64d5e668f1521ccf55e4cd264f9cf5
Linda <ltramos7@gmail.com>
2020-09-15 17:30:49 -0500

    created grid for home page styling. Need to fix how divs layer each other.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,14 +3,29 @@ import React from 'react';
 class Home extends React.Component{
     render(){
         return(
-            <section>
-                <br />
-                <br />
-                <br />
-                <br />
-                <h1 className='home'>Home Component</h1>
+            <div className='home'>
+                                
+                <div className='intro'>
+                    <h1>Better Products, Faster.</h1>
+                </div>
 
-            </section>
+                <div className='intro'>
+                {/* need to figure out better class name or combine the div above with the one I'm making a comment in. */}
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget tortor mi congue malesuada condimentu</p>
+                </div>
+
+                <div className='jumbotron'>
+                    <p>I'm guessing an image belongs here.</p>
+                </div>
+
+                <div className='hello'>
+                    <h1>Hello, Lorem Ipsum sit amet</h1>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget tortor mi congue malesuada condimentum sed vulputate. Dictum ipsum condimentum malesuada quisque sit lectus nunc feugiat. Leo, lacus eleifend vitae eget tortor hac habitasse. Lectus consectetur tempor, sit.</p>
+
+                    <p>Check out some of our favourite projects below.</p>
+                </div>
+            </div>
         )
     }
 }
