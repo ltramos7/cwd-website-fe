afbc12f25547139d34d4a2055ad042d11f0d1a05
StephanieZou1 <stephanie.zou3@gmail.com>
2020-09-28 17:14:05 -0700

    added tagline and image, need to format

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
                 <div className='home'>
                     <h1 className="home__title">Better Products, Faster.</h1>
                     <p className="home__tagline">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget tortor mi congue malesuada condimentu</p>
-                    
+                    <img className="home__wide-image" alt="chicago-wide" src={require("../../images/chicago-wide.jpg")} />
                 </div>
             </section>
         )
