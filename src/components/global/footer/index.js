cb2cdc7509a265967a372b24d73178f13a0a61ce
Alejandro Linan <jandrolinan@gmail.com>
2020-10-25 14:09:58 -0500

    footer structure and initial style set up

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -13,6 +13,34 @@ class Footer extends React.Component{
                         <img src={require(`./images/minimal-placeholder.jpg`)} alt='placeholder-image' />
                     </div>
                 </div>
+
+                <div className='footer__sub'>
+                    <div className='footer__sub-text'>
+                        <p>LOGO</p>
+                        <p>City of Wind is a design studio that specializes in UX Design and Software Development for startups</p>
+                        <p>&copy; City of Wind Design 2020</p>
+                    </div>
+
+                    <div className='footer__sub-project'>
+                        <ul className='footer__sub-project-list'>
+                            <p>Have a project for us?</p>
+                            <li>User Research</li>
+                            <li>Prototype a concept</li>
+                            <li>Ideate & Test a new product</li>
+                            <li>Improve an existing product</li>
+                        </ul>
+                    </div>
+
+                    <div className='footer__sub-site-map'>
+                        <ul className='footer__sub-site-map-list'>
+                            <p>Site Map</p>
+                            <li>About</li>
+                            <li>Works</li>
+                            <li>Blog</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                </div>
             </footer>
         )
     }
