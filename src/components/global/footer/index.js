1cf5a72d6dcd93a426a2c00ef4f6567e5ab81bbd
Alejandro Linan <jandrolinan@gmail.com>
2020-10-25 13:56:41 -0500

    footer head and rewrote variables and instances

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -3,74 +3,16 @@ import React from 'react';
 class Footer extends React.Component{
     render(){
         return(
-            <footer>
-                <ul className='top-footer'>
-                    <li >
-                        <a className='footer-logo'>
-                            CWD
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Work
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Services
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Career
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Contact
-                        </a>
-                    </li>
-                    <li className='push'>
-                        <a>
-                            0
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            0
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            0
-                        </a>
-                    </li>
-                </ul>
-
-                <hr className='footer-line' />
-                
-                <ul className='bottom-footer'>
-                    <li>
-                        <a>
-                            Terms & Conditions
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Privacy Policy
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            FAQ
-                        </a>
-                    </li>
-                </ul>
+            <footer className='footer'>
+                <div className='footer__head'>
+                    <div className='footer__head-text'>
+                        <h1>Have a Project Need?</h1>
+                        <button>Let's work together -></button>
+                    </div>
+                    <div className='footer__head-img'>
+                        <img src={require(`./images/minimal-placeholder.jpg`)} alt='placeholder-image' />
+                    </div>
+                </div>
             </footer>
         )
     }
