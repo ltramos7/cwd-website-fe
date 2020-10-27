875e93924cbb53619d379ccc731a47527fa56d99
Alejandro Linan <jandrolinan@gmail.com>
2020-10-27 14:15:38 -0500

    began blog detail and blog data detup

---
+++ src/components/blog/blog-detail/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Dodo } from '../blog-data/index'
+
+class BlogDetail extends React.Component {
+
+    componentDidMount(){
+        console.log(Dodo)
+    }
+
+    render(){
+        return(
+            <section>
+                BLOG DETAIL
+
+
+
+            </section>
+        )
+    }
+}
+
+export default BlogDetail
