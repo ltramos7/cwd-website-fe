952c5b2459fa9d2d9bb9b252fa992c2bd5cb2631
Alejandro Linan <jandrolinan@gmail.com>
2020-11-03 10:37:46 -0600

    wrkstudy - retrieves correct study based on id, also sets it in state

--- src/components/work/workstudy-detail/index.jsx
+++ src/components/work/workstudy-detail/index.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import { WorkstudyData } from '../workstudy-detail/workstudy-data/index';
 
 class WorkstudyDetail extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            set: false,
+            workstudy: null
+        }
+    }
+
+    setWorkstudyDetailState = (id) => {
+        let retrievedWorkstudy = WorkstudyData.find(study => study.id == id)
+
+        this.setState({
+            ...this.state,
+            set: true,
+            card: retrievedWorkstudy
+        })
+    }
+
+    componentDidMount(){
+        this.setWorkstudyDetailState(this.props.history.location.pathname.split('/')[2])
+    }
 
     render(){
         return(
             <section className='workstudy-detail'>
-                Workstudy
+                {/* { this.state.set ? this.returnHeadCard() : <p>Loading...</p>} */}
+
+                {/* { this.state.set ? this.returnHeadImg() : null }
+
+                { this.state.set ? this.returnMainContent() : null }
+
+                { this.state.set ? this.returnImages() : null }
+
+                { this.state.set ? this.returnLastContent() : null }
+
+                { this.state.set ? this.returnLastImage() : null } */}
+                
             </section>
         )
     }
