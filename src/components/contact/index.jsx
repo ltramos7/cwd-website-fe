9b11d7a77f3fdfa6e3a83b1a47114558bb2da95f
Alejandro Linan <jandrolinan@gmail.com>
2020-11-11 11:02:27 -0600

    contact form styling

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -15,17 +15,17 @@ class Contact extends React.Component{
     render(){
         return(
             <section classname='contact'>
-                <div className='contact-container'>
+                <div className='contact__heading'>
                     <h1>Let's work together</h1>
                     <p>We told you our names, now tell us yours!</p>
                 </div>
 
                 <div>
-                    <form>
-                        <input type='text' placeholder='My name is' />
-                        <input type='text' placeholder='You can email me at' />
-                        <textarea type='text' placeholder="I've got something to say" />
-                        <button>Send now --></button>
+                    <form className='contact__form'>
+                        <input className='contact__form-text' type='text' placeholder='My name is' />
+                        <input className='contact__form-text' type='text' placeholder='You can email me at' />
+                        <textarea className='contact__form-text-area' type='text' placeholder="I've got something to say" />
+                        <button className='contact__form-btn'>Send now --></button>
                     </form>
                 </div>
             </section>
