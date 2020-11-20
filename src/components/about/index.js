6480f998ea7905fdbe0a642600bc43ed745d088e
Auden Robertson <>
2020-11-19 21:29:45 -0500

    updated about page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,59 +8,112 @@ class About extends React.Component{
             teamNum: 0,
             teamBios: [
                 {
-                    name: 'Person One',
-                    title: 'Product Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'David Pinedo',
+                    title: 'Lead UX Designer',
+                    
+                    email: 'david@cityofwinddesign.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Two',
-                    title: 'Founder, Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Valentine Ubaldo',
+                    title: 'Lead UX Designer',
+                    email: 'vtine.ubaldo@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Three',
-                    title: 'Co Founder, Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Alexandra Grochowski',
+                    title: 'UX/UI Designer',
+                    email: 'agrochowski132@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Four',
-                    title: 'Product Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Chelsea Kennedy',
+                    title: 'UX/UI Designer',
+                    email: 'chelsea.aline.kennedy@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Five',
-                    title: 'Founder, Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Pippa Nardie-Warner',
+                    title: 'UX/UI Designer',
+                    email: 'pnardiewarner@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Six',
-                    title: 'Co Founder, Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Alejandro Linan',
+                    title: 'Lead In House Software Engineer',
+                    email: 'jandrolinan@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Seven',
-                    title: 'Product Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Ingrid Pohl',
+                    title: 'UX Researcher',
+                    email: 'ingpohl@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 },
                 {
-                    name: 'Person Eight',
-                    title: 'Founder, Designer',
-                    phone: '+1 (773) 123-1231',
-                    email: 'kala.anshika@cwd.com',
+                    name: 'Stephanie Zou',
+                    title: 'Full Stack Developer',
+                    email: '',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Nora Villar',
+                    title: 'Service Designer',
+                    email: 'noravillarc@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Titania Veda',
+                    title: 'lead Service Designer',
+                    email: 'vedaimpactdesigns@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Andrea Wilson',
+                    title: 'Service Designer',
+                    email: 'andreawi2@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Ayesha Ather',
+                    title: 'UX Researcher',
+                    email: 'ayesha.ath@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Summer',
+                    title: 'UX/UI Designer',
+                    email: 'summer.elliott.ux@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Alexandra Sevareid',
+                    title: 'UI Designer',
+                    email: 'alexandra@sevareid.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Celia Kim',
+                    title: 'UX/UI Designer',
+                    email: 'hi@celiaskim.design',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'June Tang',
+                    title: 'UX/UI Designer',
+                    email: 'ttkhanhdung@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Vanessa Guerrero Barragan',
+                    title: 'Design Intern',
+                    email: 'vamarilug@gmail.com',
+                    image: './images/rocky-wallpaper.jpg'
+                },
+                {
+                    name: 'Auden Robertson',
+                    title: 'Full Stack Developer',
+                    email: 'audenrobertson@gmail.com',
                     image: './images/rocky-wallpaper.jpg'
                 }
             ]
@@ -79,7 +132,6 @@ class About extends React.Component{
                         <img className='about__team-img' src={require(`${person.image}`)} alt='placeholder' />
                         <h5 className='about__team-member-name'>{person.name}</h5>
                         <p className='about__team-member-job'>{person.title}</p>
-                        <p className='about__team-member-phone'>{person.phone}</p>
                         <p className='about__team-member-email'>{person.email}</p>
                     </div>
                 )
@@ -127,12 +179,17 @@ class About extends React.Component{
     render(){
         return(
             <section className='about'>
-                <h1 className='about__main-title'>ABOUT</h1>
+
+                <div className='about__main-title'>
+                    <h2>In the City of Wind...</h2>
+                </div>
 
                 <div className='about__intro'>
-                    <h3 className='about__intro-title'>
-                        What we believe
-                    </h3>
+                    <div className='about__intro-title'>
+                        <h4>What we<br></br> 
+                        believe
+                        </h4>
+                    </div>
                     <div className='about__intro-info'>
                         <p>
                             viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis
@@ -173,6 +230,12 @@ class About extends React.Component{
 
                 <div>
                     <h3 className='about__services-title'>Services</h3>
+                    <div className='about__services_slides'>
+                        <img className='about__services_slides-one' src={require(`./images/rocky-wallpaper.jpg`)} alt='placeholder' />
+                        <img className='about__services_slides-two' src={require(`./images/rocky-wallpaper.jpg`)} alt='placeholder' />
+                        <img className='about__services_slides-three' src={require(`./images/rocky-wallpaper.jpg`)} alt='placeholder' />
+                    </div>
+                    
                     <div className='about__services'>
                         <div className='about__services-one'>
                             <h5 className='about__services-name'>Strategy & Organization</h5>
@@ -189,15 +252,11 @@ class About extends React.Component{
                             <p className='about__services-description'>lorem this is the description la lorem i dont have it for react lorem</p>
                             <p className='about__services-read'>Read More</p>
                         </div>
-                        <div className='about__services-four'>
-                            <h5 className='about__services-name'>Lotem Ipsum & Sit</h5>
-                            <p className='about__services-description'>lorem this is the description la lorem i dont have it for react lorem</p>
-                            <p className='about__services-read'>Read More</p>
-                        </div>
                     </div>
+                    
                 </div>
 
-                <div className='about__contact'>
+                {/* <div className='about__contact'>
                     <h1 className='about__contact-us'>Contact Us</h1>
                     <div className='about__contact-text'>
                         <p>
@@ -209,7 +268,7 @@ class About extends React.Component{
                             <li>lorem ipsum dolor sit amet</li>
                         </ul>
                     </div>
-                </div>
+                </div> */}
             </section>
         )
     }
